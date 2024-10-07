409e38b2c40c5bfc134a5b6aa8b4b37fc8916344
kzy55555 <dd22113@stumail.daido-it.ac.jp>
2024-10-07 11:15:55 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>good CodeSandbox</h1>
+      <h2>Start editing to How Many Boogie!</h2>
+    </div>
+  );
+}
